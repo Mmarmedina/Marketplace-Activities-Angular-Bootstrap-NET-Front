1778981703b74590c6d9168e0e4d01707bf18267
Mmarmedina <maria.martin.84@outlook.es>
2024-03-25 21:38:23 +0100

    Update activity form: set default form values.  All inputs from checkbox are selected by default (schedules). It doesn't work yet.

--- Sprencia-Activities-Marketplace/src/app/components/form-update-activity/form-update-activity.component.ts
+++ Sprencia-Activities-Marketplace/src/app/components/form-update-activity/form-update-activity.component.ts
@@ -4,7 +4,7 @@ import { Schedule } from '../../interfaces/schedule.interface';
 import { ActivitiesService } from '../../services/activities.service';
 import { SchedulesService } from '../../services/schedules.service';
 import { ActivatedRoute } from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-update-activity',
@@ -12,7 +12,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './form-update-activity.component.css'
 })
 export class FormUpdateActivityComponent {
-  updateActivityForm!: FormGroup;
+  updateActivityForm: FormGroup;
   activity: Activity | undefined;
   allSchedules: Schedule[];
   selectedSchedules: number[];
@@ -21,8 +21,16 @@ export class FormUpdateActivityComponent {
     private activitiesService: ActivitiesService,
     private activatedRoute: ActivatedRoute,
     private schedulesService: SchedulesService){
+
       this.allSchedules = [];
-      this.selectedSchedules = [];
+      this.selectedSchedules = []
+
+      this.updateActivityForm = new FormGroup({
+        title: new FormControl('', [Validators.required, Validators.minLength(25), Validators.maxLength(130)]),
+        description: new FormControl('', [Validators.required, Validators.minLength(100)]),
+        price: new FormControl('', [Validators.required]),
+        schedule: new FormControl('', [Validators.requiredTrue]),
+      });    
   }
 
   async ngOnInit() {
@@ -31,15 +39,19 @@ export class FormUpdateActivityComponent {
     this.getAllSchedules();
 
     // Cargar el formulario y sus validaciones.
-    this.initializeForm();
+    // this.initializeForm();
 
     this.activatedRoute.params.subscribe(async (params: any) => {
       const id = parseInt(params.idactivity);
       this.activity = await this.activitiesService.getById(params.idactivity);
       console.log(this.activity);
       console.log (this.activity.schedule);
-
+      
       this.setDefaultFormValues();
+      
+      // Marcar checkboxes después de establecer los valores por defecto en el formulario
+      
+      
     })
 
   }
@@ -56,27 +68,39 @@ export class FormUpdateActivityComponent {
   }
 
   // Formulario y sus validaciones.
-  initializeForm(){
-    this.updateActivityForm = new FormGroup({
-      title: new FormControl('', [Validators.required, Validators.minLength(25), Validators.maxLength(130)]),
-      description: new FormControl('', [Validators.required, Validators.minLength(100)]),
-      price: new FormControl('', [Validators.required]),
-      schedule: new FormControl('', [Validators.requiredTrue]),
-    });
-  }
+  // initializeForm(){
+  //   this.updateActivityForm = new FormGroup({
+  //     title: new FormControl('', [Validators.required, Validators.minLength(25), Validators.maxLength(130)]),
+  //     description: new FormControl('', [Validators.required, Validators.minLength(100)]),
+  //     price: new FormControl('', [Validators.required]),
+  //     schedule: new FormControl('', [Validators.requiredTrue]),
+  //   });
+  // }
 
-  // Establecer valores por defecto en el formulario
-  setDefaultFormValues() {
+
+  setDefaultFormValues(): number[] | undefined {
+   
+    // Asignar valores por defecto
     if (this.activity) {      
       console.log (this.activity);
+      console.log (this.activity.schedule);
+
       this.updateActivityForm.patchValue({
         title: this.activity.title,
         description: this.activity.description,
         price: this.activity.price,
-        schedule: this.activity.schedule
-      });
-    }     
+        // schedule: selectedScheduleIds        
+      }); 
+
+    }
+
+    // Quedarnos solo con schedule.id, array de ids con los horarios (porque activity.schedule, incluye el id del horario y su name)
+    const selectedScheduleIds: number[] | undefined = this.activity?.schedule.map(schedule => schedule.id);
+    console.log(selectedScheduleIds);
+    
+    return selectedScheduleIds;
   }
+
 
   // Que si cambian los valores del checkbox se actualice el valor de schedules en el formulario.
   updateSelectedSchedules (event: Event, schedule: Schedule): number []{
@@ -91,10 +115,11 @@ export class FormUpdateActivityComponent {
     console.log(this.selectedSchedules);
     return this.selectedSchedules;  
   }
-
+  
+  
   updateFormControlScheduleValue(selectedSchedules: number []) {
     this.updateActivityForm.get('schedule')?.setValue(this.selectedSchedules);
-}
+  }
 
   // MMM Método para mostrar el mensaje de error cuando no se rellena correctamente el campo.
   checkError (control: string, error: string) {
